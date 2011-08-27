013fb97fb6a9cffde4e9be4feb2d8910872b9336
Chris Gough <csgough@gmail.com>
2011-08-27 16:43:03 -0700

    completed chapter 8 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-      if @user.save
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
-      else
-        @title = "Sign up"
-        render 'new'
-      end
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @user.password = ""
+      @title = "Sign up"
+      render 'new'
+    end
+
   end
 
 end
